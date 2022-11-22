5ce7d86ba3dc5d41233a5cf77036e61c7c211d03
Artur <artur180892@mail.ru>
2022-11-22 17:43:49 +0300

    initial commit

---
+++ app/main.py
@@ -0,0 +1,29 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.routers import auth, post, user, vote
+
+
+# models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+origins = ["*"]  # public API
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # domains which are able to talk to our API
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+app.include_router(auth.router)
+app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(vote.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", reload=True)
