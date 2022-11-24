f540bf760073f1235354fde9c0077e6cf0fe2aa9
Artur <artur180892@mail.ru>
2022-11-24 23:47:07 +0300

    add test database setup

--- app/main.py
+++ app/main.py
@@ -25,5 +25,10 @@ app.include_router(user.router)
 app.include_router(vote.router)
 
 
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
 if __name__ == "__main__":
     uvicorn.run("app.main:app", reload=True)
